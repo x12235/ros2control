d012c53948696fdf5ee879c5cb1e1a2c8a013aa9
Kitili <54706322+x12235@users.noreply.github.com>
2024-12-10 13:28:51 +0000

    Add files via upload

--- Control/turtle/launch/Rlaunch.py
+++ Control/turtle/launch/Rlaunch.py
@@ -15,57 +15,62 @@ from launch.event_handlers import OnProcessExit
 def generate_launch_description():
     # Constants for paths to different files and folders
     models_path = 'meshes'
-    package_name = 'turtle_on_land_demo'
+    package_name = 'turtle'
     urdf_name='URDF1.urdf.xacro'
-    world_sdf_file = 'world.sdf'
+    world_sdf_file = 'fws_robot_world.sdf'
 
-    urdf=os.path.join(get_package_share_directory('turtle_on_land_demo'),'urdf',urdf_name)
+    #Set paths to different files and packages
+    urdf=os.path.join(get_package_share_directory(package_name),'urdf',urdf_name)
     rviz_config_file='rviz/rviz_basic_settings.rviz'
-    path_to_world_sdf = os.path.join(
-        get_package_share_directory(package_name),
-        'world',
-        world_sdf_file)
-
-
-    gz_args = f"-r -v 4 {path_to_world_sdf}"
-
-    # Set paths to different files
+    path_to_world_sdf = os.path.join( get_package_share_directory(package_name),'world', world_sdf_file)
+    resource_path = get_package_share_directory(package_name)
     pkg_gazebo_ros = FindPackageShare(package='ros_gz_sim').find('ros_gz_sim')
     pkg_share = FindPackageShare(package=package_name).find(package_name)
 
+    #Gazebo models path
     gazebo_models_path = os.path.join(pkg_share,models_path)
     os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
-    resources=os.path.join(get_package_share_directory('turtle_on_land_demo'))
+
+    #Rviz config file
     rviz_config_path=os.path.join(pkg_share,rviz_config_file)
 
+    #URDF Parser
     doc = xacro.process_file(urdf, mappings={'use_sim' : 'true'})
 
+    #Robot description argument
     robot_desc = doc.toprettyxml(indent='  ')
     robot_description = {"robot_description": robot_desc}
+   
     # Nodes
+    #Robot_state_publisher Node
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[robot_description]
+        parameters=[robot_description],
     )
 
+    #Joint_state_publisher Node
     joint_state_publisher_node=Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
     )
 
-    # Launch RViz2
+    #Rviz Node; Launches RVIZ
     launch_rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
-        arguments=["-d", rviz_config_path]
+        arguments=["-d", rviz_config_path],
+        parameters=[
+        {'use_sim_time': True},  # Ensure simulated time is used
+        {'tf_buffer_size': 120.0},  # Increase transform buffer size
+     ]
     )
 
-    # Launch Gazebo simulator with an empty world
+    # Gazebo Node: Launches Gazebo
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
@@ -77,6 +82,7 @@ def generate_launch_description():
         launch_arguments={"gz_args": f"-r -v 4 {path_to_world_sdf}"}.items(),
     )
 
+    #Spawns entity Node
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
@@ -93,75 +99,89 @@ def generate_launch_description():
     )
 
     # Set environment variable for GZ_SIM_RESOURCE_PATH
-    resource_path = get_package_share_directory('turtle_on_land_demo')
-    set_gz_sim_resource_path = SetEnvironmentVariable(
-        name='GZ_SIM_RESOURCE_PATH',
-        value=resource_path
-    )
+    
 
+    #Gazebo resources
     set_env_vars_resources2 = AppendEnvironmentVariable(
             'GZ_SIM_RESOURCE_PATH',
             str(Path(os.path.join(resource_path)).parent.resolve()))
 
+    #Joint Vel Controller Node; Initiazlizes joint velocity control
     Velcoity_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_vel"],
+        parameters=[{'use_sim_time': True}],
     )
 
+    #Joint state broadcaster Node; Initializes Joint state broadcaster
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster"],
+        parameters=[{'use_sim_time': True}],
     )
     
 
-    # Add the ros_gz_bridge node to bridge the joint states and commands
+    # Bridge Node: Initializes lidar scan topic
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        name='ros_gz_bridge',
-        arguments=[
-            # Bridge joint states from ROS 2 to Gazebo transport
-            #'/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
-            
-            # Bridge velocity commands if controlling with velocity
-            #'/cmd_vel@geometry_msgs/msg/Twist[gz.msgs.Twist',
-            
-            #'/lidar@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
-            '/lidar/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked',
-
-            '/camera1_image@sensor_msgs/msg/Image[gz.msgs.Image',
-
-            #'/camera1_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
-
-            '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
-
-            #'/model/robot1/pose@geometry_msgs/msg/Odometry[gz.msgs.Pose',
-
-            #'/joint_states@geometry_msgs/msg/Transform[gz.msgs.Pose',
-
-            '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU'
-            # {
-            #     'config_file': os.path.join(
-            #         get_package_share_directory('landTurtle2'), 'configs', 'landturtle_bridge.yaml'
-            #     ),
-            #     #'expand_gz_topic_names': True,
-            #     'use_sim_time': True,
-            # }
-        ],
+        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
+        output='screen',
+        parameters=[{'use_sim_time': True}],
     )
      
-
+    #3 second delayed velocity spawner
     delayed_velocity= TimerAction(
         period=3.0,  # Delay in seconds
         actions=[Velcoity_spawner],
     )
 
+    #2.5 second delayed joint state broadcaster spawner
     delayed_joint= TimerAction(
         period=2.5,  # Delay in seconds
         actions=[joint_broad_spawner],
     )
+    
+
+    velocity_joint_publisher= Node(
+        package=package_name,
+        executable='velocity_publisher.py',
+        name='vel_publisher',
+        output='screen',
+    )
+
+    h_slam_node= Node(
+        package= package_name,
+        executable='hslam.py',
+        name='slam',
+        output='screen',
+    )
+
+    odometry_publisher=Node(
+        package=package_name,
+        executable='odometry_publisher.py',
+        name='odom_publish',
+        output='screen',
+    )
+
+    delayed_publisher= TimerAction(
+        period=4.0,  #4 Second Delay
+        actions=[velocity_joint_publisher],
+    )
+
+    delayed_odom=TimerAction(
+        period=5.0, #4 Second Delay
+        actions=[odometry_publisher],
+    )
+
+    delayed_slam=TimerAction(
+        period=5.0,
+        actions=[h_slam_node],
+    )
+
+    
 
     return LaunchDescription([
         set_env_vars_resources2,
@@ -173,5 +193,8 @@ def generate_launch_description():
         delayed_velocity,
         delayed_joint,
         bridge,
+        delayed_publisher,
+        h_slam_node,
+        delayed_odom,
         
     ])
